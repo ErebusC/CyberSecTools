2c054407b69215aa9f671fe4bd6aea407a5476b8
Daniel Roberts <danny@erebus.cymru>
2024-02-13 15:07:35 +0000

    Created command line function to instantly insert the necessary website into the iframe and improved the CSS.

--- go_clickjack/clickjack.go
+++ go_clickjack/clickjack.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"net/http"
+	"html/template"
+	"os"
 )
 
+type website struct {
+	Site string
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./website")))
-	http.ListenAndServe(":9999", nil)
+	args := os.Args
+
+	if len(args) == 2 {
+		tmpl := template.Must(template.ParseFiles("website/index.html"))
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			data := website{
+				Site: args[1],
+			}
+
+			tmpl.Execute(w, data)
+		})
+		http.ListenAndServe(":9999", nil)
+	} else {
+		http.Handle("/", http.FileServer(http.Dir("./website")))
+		http.ListenAndServe(":9999", nil)
+	}
+	
 }
