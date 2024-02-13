f60076491ca7a4132ff4edb4446339e0c8013738
Daniel Roberts <danny@erebus.cymru>
2024-02-13 15:51:23 +0000

    Fixed issue where CSS was not getting loaded and attempted to add browser opening functionality

--- go_clickjack/clickjack.go
+++ go_clickjack/clickjack.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"html/template"
 	"os"
+	"log"
+	"runtime"
+	"os/exec"
 )
 
 type website struct {
@@ -22,10 +26,35 @@ func main() {
 
 			tmpl.Execute(w, data)
 		})
+
+		http.Handle("/website/", http.StripPrefix("/website/", http.FileServer(http.Dir("website"))))
 		http.ListenAndServe(":9999", nil)
 	} else {
 		http.Handle("/", http.FileServer(http.Dir("./website")))
 		http.ListenAndServe(":9999", nil)
 	}
+
+	openBrowser("http://localhost:9999")
 	
+}
+
+func openBrowser(targetURL string) {
+    var err error
+
+    switch runtime.GOOS {
+    case "linux":
+        err = exec.Command("xdg-open", targetURL).Start()
+        // TODO: "Windows Subsytem for Linux" is also recognized as "linux", but then we need
+        // err = exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", targetURL).Start()
+    case "windows":
+        err = exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", targetURL).Start()
+    case "darwin":
+        err = exec.Command("open", targetURL).Start()
+    default:
+        err = fmt.Errorf("unsupported platform %v", runtime.GOOS)
+    }
+    if err != nil {
+        log.Fatal(err)
+    }
+
 }
