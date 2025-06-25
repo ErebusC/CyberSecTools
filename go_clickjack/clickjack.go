987371c0a553e5eb95b70bd3ef6e8959c512fe60
Danny <danny@erebus.cymru>
2025-06-25 17:13:52 +0100

    Started adding functionality to allow for quick PoC attacks

--- go_clickjack/clickjack.go
+++ go_clickjack/clickjack.go
@@ -13,6 +13,7 @@ import (
 
 type website struct {
 	Site string
+	Collab string
 }
 
 var (
@@ -25,37 +26,38 @@ var (
 )
 
 func main() {
+	args := os.Args
 	envVariable := os.Getenv("CONTAINER")
 
 	if envVariable == "TRUE"{
-		route()
+		route(args)
 	} else {
 		openBrowser("http://localhost:9999")
-		route()
+		route(args)
 	}
 }
 
-func route(){
-	args := os.Args
+func route(args []string){
 
 	var staticFs = http.FS(staticFS)
 	staticFile := http.FileServer(staticFs)
 
 	http.Handle("/static/", staticFile)
 
-	if len(args) == 2 {
-		frameResponse(args[1])
+	if len(args) >= 2 {
+		frameResponse(args[1], args[2])
 	} else {
-		frameResponse("https://bing.co.uk")
+		frameResponse("https://economist.com", "")
 	}
 }	
 
-func frameResponse(targetURL string) {
+func frameResponse(targetURL string, targetCollab string) {
     tmpl := template.Must(template.ParseFS(templatesFS, "html/index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Frame-Options", "Deny")
 		data := website{
 			Site: targetURL,
+			Collab: targetCollab,
 		}
 
 		tmpl.Execute(w, data)
@@ -64,14 +66,13 @@ func frameResponse(targetURL string) {
 }
 
 
+
 //This was taken from here; https://code-maven.com/slides/golang/open-web-browser. 
 func openBrowser(targetURL string) {
     var err error
     switch runtime.GOOS {
     case "linux":
         err = exec.Command("xdg-open", targetURL).Start()
-        // TODO: "Windows Subsytem for Linux" is also recognized as "linux", but then we need
-        // err = exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", targetURL).Start()
     case "windows":
         err = exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", targetURL).Start()
     case "darwin":
